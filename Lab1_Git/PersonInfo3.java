eed0e799cc66ab52ad1e0f89a33f12adc4a44605
stephaniejazzar <sjazz031@uottawa.ca>
2019-09-17 17:43:58 -0400

    modification 1

--- Lab1_Git/PersonInfo3.java
+++ Lab1_Git/PersonInfo3.java
@@ -1,9 +1,9 @@
 public class PersonInfo3 {
 
-    public static String firstName = "Liz";
-    public static String lastName = "Grégoire";
-    public static int studentNo = 173819;
-    public static String description = "Likes long walks along the beach";
+    public static String firstName = "Stephanie";
+    public static String lastName = "Jazzar";
+    public static int studentNo = 8141726;
+    public static String description = "Software Engineer student";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
@@ -11,18 +11,18 @@ public class PersonInfo3 {
         out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
         out += SIConstants.DESCRIPTION_FR + description + '.';
         out += SIConstants.EOL;
-        
+
         System.out.println(out);
     }
 
-    
+
     public static void displayEn() {
         String out = SIConstants.GREETING_EN;
         out += " " + firstName + " " + lastName + '.';
         out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
         out += SIConstants.DESCRIPTION_EN + description + '.';
         out += SIConstants.EOL;
-        
+
         System.out.println(out);
     }
 }
